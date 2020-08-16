0c431d11acd55702f793a77076eb2afe1db76ec4
Maria Medina <mariescuela@outlook.com>
2020-08-16 18:38:09 -0300

    ¡¡Preg si junto o separado!!

---
+++ TP1/ejercicio1.js
@@ -0,0 +1,11 @@
+//VER SI DEJAR ASI, SEPARADO DEL RESTO DE LOS ARCHIVOS
+//O JUNTO CON LOS DEMAS
+
+//Ejercicio 1 - Concatenación entre 2 cad literales para devolver edad 
+const edad = "22"
+const nombre = "Maria"
+console.log(`${nombre} tiene ${edad} años`);
+
+const edad1 = "25"
+const nombre1 = "Juan"
+console.log(`${nombre1} tiene ${edad1} años`);
---
+++ TP1/ejercicio2.js
@@ -0,0 +1,8 @@
+//Ejercicio 2 - Operaciones artimeticas
+let n1 = "25"
+let n2 = "5"
+
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
