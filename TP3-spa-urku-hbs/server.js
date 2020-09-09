fcb023c06e2bdcb700bb7f366772962dac0a2d81
marimedina <mariescuela@outlook.com>
2020-09-09 14:46:51 -0300

    Terminado

--- TP3-spa-urku-hbs/server.js
+++ TP3-spa-urku-hbs/server.js
@@ -14,5 +14,20 @@ app.get('/', (req, res) => {
     res.render('index')
 })
 
+app.get('/portfolio', (req, res) => {
+    res.render('portfolio')
+})
+
+app.get('/blog', (req, res) => {
+    res.render('blog')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
 app.listen(port);
 console.log(`Server escuchando en http://localhost:${port}/`);
