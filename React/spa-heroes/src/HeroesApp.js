1f30fee746432f21dda23d1c613d6cb77e2589c7
marimedina <mariescuela@outlook.com>
2020-11-09 12:57:44 -0300

    Incompleto

---
+++ React/spa-heroes/src/HeroesApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Components/UI/Navbar';
+
+const Heroes = () => {
+    return ( 
+        <div>
+            <>
+                <Navbar/>
+            </>
+        </div>
+    );
+}
+    
+export default Heroes;
---
+++ React/spa-heroes/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HeroesApp from '../src/HeroesApp';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HeroesApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
